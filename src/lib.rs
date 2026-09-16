914a02f910e16eabbef481a1af4952ea4310296f
Janos Lazar <lazarjanos77@gmail.com>
2026-09-16 15:07:17 +0200

    feat: implement keyed BLAKE3 chunk hashing

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,11 @@
 mod chunk_boundary;
+mod chunk_hash;
 mod chunker;
 mod gear_hash;
 mod protocol;
 
 pub use chunk_boundary::ChunkBoundary;
+pub use chunk_hash::{ChunkHash, hash_chunk};
 pub use chunker::Chunker;
 pub use gear_hash::GearHash;
 pub use protocol::{BOUNDARY_MASK, MAX_CHUNK_SIZE, MIN_CHUNK_SIZE, TARGET_CHUNK_SIZE};
